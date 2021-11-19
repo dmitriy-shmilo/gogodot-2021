3838d08c7c694eea2f13c86275d0b41c6b751e27
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-11-19 22:28:13 +0200

    Enemy unit state.

--- src/game_screen/entities/enemy_unit.gd
+++ src/game_screen/entities/enemy_unit.gd
@@ -2,11 +2,11 @@ extends KinematicBody2D
 class_name EnemyUnit
 
 enum EnemyState {
-	STATE_IDLE,
-	STATE_MOVING,
-	STATE_AGGRAVATED,
-	STATE_ATTACKING,
-	STATE_DEAD
+	IDLE,
+	MOVING,
+	AGGRAVATED,
+	ATTACKING,
+	DEAD
 }
 onready var line: Line2D = $"../Line2D"
 
@@ -19,7 +19,12 @@ var _distance_threshold = 5
 var _target_point_index = 0
 var _total_points = 0
 var _points: PoolVector2Array = PoolVector2Array()
-var _state = EnemyState.STATE_IDLE
+var _state = EnemyState.IDLE
+
+
+func is_threat() -> bool:
+	return _state != EnemyState.DEAD
+
 
 func _ready() -> void:
 	_points = line.points
@@ -27,13 +32,55 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:
+	match _state:
+		EnemyState.MOVING:
+			_waypoint_move()
+		EnemyState.IDLE:
+			_move_to_state(EnemyState.MOVING)
+
+
+func _waypoint_move() -> void:
 	if _total_points == 0:
 		return
 
 	var target = _points[_target_point_index]
 	
 	if position.distance_to(target) < _distance_threshold:
-		_target_point_index = (_target_point_index + 1) % _total_points
+		_target_point_index = _target_point_index + 1
+	
+	if  _target_point_index >= _total_points:
+		_move_to_state(EnemyState.DEAD)
 	
 	_velocity = (target - position).normalized() * move_speed
 	_velocity = move_and_slide(_velocity)
+
+
+func _can_move_to_state(state: int) -> bool:
+	match state:
+		EnemyState.IDLE:
+			return false
+		EnemyState.MOVING:
+			return _state == EnemyState.AGGRAVATED \
+				or _state == EnemyState.ATTACKING \
+				or _state == EnemyState.IDLE
+		EnemyState.AGGRAVATED:
+			return _state == EnemyState.MOVING \
+				or _state == EnemyState.IDLE
+		EnemyState.ATTACKING:
+			return _state == EnemyState.MOVING \
+				or _state == EnemyState.AGGRAVATED \
+				or _state == EnemyState.IDLE
+		EnemyState.DEAD:
+			return _state != EnemyState.DEAD
+		_:
+			printerr("Unknown state %d", state)
+			return false
+
+
+func _move_to_state(state: int) -> void:
+	if _can_move_to_state(state):
+		_state = state
+
+
+func _describe_state():
+	return EnemyState.keys()[_state]
