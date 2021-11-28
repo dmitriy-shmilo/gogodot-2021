dcb49f19cc9903f9c3b25546a4312603a625ca0c
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-11-28 22:00:49 +0200

    Edge scrolling camera.

--- src/game_screen/camera.gd
+++ src/game_screen/camera.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const HALF_RES_X = 640 / 2
+const HALF_RES_Y = 360 / 2
+
 export(float) var speed = 400
 export(float) var friction = 0.15
 export(float) var acceleration = 0.5
@@ -9,10 +12,6 @@ onready var _camera = $"Camera"
 var _velocity: Vector2 = Vector2.ZERO
 var _pan_dir: Vector2 = Vector2.ZERO
 
-func _ready() -> void:
-	pass
-
-
 func _unhandled_input(event: InputEvent) -> void:
 
 	if event is InputEventPanGesture:
@@ -20,33 +19,36 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	if event is InputEventMagnifyGesture:
-		_camera.zoom.x *= event.get_factor()
-		_camera.zoom.y = _camera.zoom.x
+		_zoom(_camera.zoom.x * event.get_factor())
 		return
 
 	if event.is_action_pressed("zoom_in"):
-		_camera.zoom.x -= 0.25
-		_camera.zoom.y -= 0.25
+		_zoom(_camera.zoom.x - 0.25)
 		return
 
 	if event.is_action_pressed("zoom_out"):
-		_camera.zoom.x += 0.25
-		_camera.zoom.y += 0.25
+		_zoom(_camera.zoom.x + 0.25)
 		return
 
 
 func get_input():
+	
+		
 	if _pan_dir != Vector2.ZERO:
 		return _pan_dir
-
+	
+	var mt_x = HALF_RES_X * 0.98 * _camera.zoom.x
+	var mt_y = HALF_RES_Y * 0.98 * _camera.zoom.y
 	var input = Vector2()
-	if Input.is_action_pressed("right"):
+	var mouse_pos = get_local_mouse_position()
+
+	if mouse_pos.x > mt_x or Input.is_action_pressed("right"):
 		input.x += 1
-	if Input.is_action_pressed("left"):
+	if mouse_pos.x < -mt_x or Input.is_action_pressed("left"):
 		input.x -= 1
-	if Input.is_action_pressed("down"):
+	if mouse_pos.y > mt_y or Input.is_action_pressed("down"):
 		input.y += 1
-	if Input.is_action_pressed("up"):
+	if mouse_pos.y < -mt_y or Input.is_action_pressed("up"):
 		input.y -= 1
 
 	input = input.normalized()
@@ -64,3 +66,8 @@ func _physics_process(_delta) -> void:
 	else:
 		_velocity = lerp(_velocity, Vector2.ZERO, friction)
 	_velocity = move_and_slide(_velocity)
+
+
+func _zoom(level: float) -> void:
+	_camera.zoom.x = level
+	_camera.zoom.y = level
