06fe41c955af2a98528f949084b2b58cd1d2b5a0
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-11-29 19:07:53 +0200

    Display core destruction stages.

--- src/game_screen/entities/player_core.gd
+++ src/game_screen/entities/player_core.gd
@@ -1,6 +1,17 @@
 extends Node2D
 class_name PlayerCore
 
+const FRAME_SIZE = 150
+const TOTAL_FRAMES = 4
+
 export(float) var total_hitpoints = 100
 
-var current_hitpoints = total_hitpoints
+var current_hitpoints = total_hitpoints setget set_hitpoints
+
+onready var _core_sprite: Sprite = $"CoreSprite"
+
+func set_hitpoints(value: float) -> void:
+	var frame = TOTAL_FRAMES - floor(value * TOTAL_FRAMES / total_hitpoints) - 1
+	_core_sprite.region_rect.position.x = FRAME_SIZE * frame
+	current_hitpoints = value
+	
