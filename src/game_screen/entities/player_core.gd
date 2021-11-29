f6d7eef863d3f3efb1bd5c0e0c0155a79eaa65b5
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-11-29 23:07:57 +0200

    Fix core sometimes disappearing upon destruction.

--- src/game_screen/entities/player_core.gd
+++ src/game_screen/entities/player_core.gd
@@ -11,6 +11,7 @@ var current_hitpoints = total_hitpoints setget set_hitpoints
 onready var _core_sprite: Sprite = $"CoreSprite"
 
 func set_hitpoints(value: float) -> void:
+	value = clamp(value, 0, total_hitpoints)
 	var frame = TOTAL_FRAMES - floor(value * TOTAL_FRAMES / total_hitpoints) - 1
 	_core_sprite.region_rect.position.x = FRAME_SIZE * frame
 	current_hitpoints = value
